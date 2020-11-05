90528a06f80bd55b26bf41d799d8ae1c08668b71
hunz <zn000h@gmail.com>
2020-11-05 10:12:15 +0100

    switch to software i2c implementation - due to i2c peripheral errata listed in ES0243 rev 3

--- firmware/platform/stm32f042k.c
+++ firmware/platform/stm32f042k.c
@@ -55,6 +55,10 @@ void gpio_setval(uint8_t gpio_id, uint8_t val) {
 		gpio_clear(gpio_map[gpio_id][0], gpio_map[gpio_id][1]);
 }
 
+#define I2C_PORT     GPIOB
+#define I2C_SCL      GPIO6
+#define I2C_SDA      GPIO7
+
 static void gpio_setup(void) {
 	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO3 | GPIO10);          /* RXDs */
 	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE,   GPIO5 | GPIO7 | GPIO9);   /* UART1, SPI1 */
@@ -69,8 +73,9 @@ static void gpio_setup(void) {
 	gpio_set(GPIOA, GPIO1);
 
 	/* I2C1 */
-	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO6|GPIO7);
-	gpio_set_af(GPIOB, GPIO_AF1, GPIO6|GPIO7);
+	gpio_set_output_options(I2C_PORT, GPIO_OTYPE_OD, GPIO_OSPEED_2MHZ, I2C_SCL|I2C_SDA);
+	gpio_mode_setup(I2C_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, I2C_SCL|I2C_SDA);
+	gpio_clear(I2C_PORT, I2C_SCL|I2C_SDA);
 
 	/* TFT nRST, A0 */
 	gpio_mode_setup(GPIOF, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0 | GPIO1);
@@ -97,12 +102,10 @@ void msleep(uint16_t val) {
 	wait_until(jiffies + ((val+(MSEC-1))/MSEC) + 1);
 }
 
-#if 0
 static void udelay(uint32_t n) {
 	while(n--)
 		__asm__("nop");
 }
-#endif
 
 static void clock_setup(void) {
 	rcc_clock_setup_in_hsi_out_48mhz();
@@ -131,189 +134,73 @@ static void spi_setup(void) {
 	spi_enable(SPI1);
 }
 
-static void i2c_setup(void) {
-	rcc_set_i2c_clock_sysclk(I2C1);
-	i2c_peripheral_disable(I2C1);
-	i2c_enable_analog_filter(I2C1);
-	i2c_set_digital_filter(I2C1, 0);
-	i2c_set_speed(I2C1, i2c_speed_sm_100k, 48);
-	i2c_peripheral_enable(I2C1);
-	i2c_disable_autoend(I2C1); /* disable auto STOP generation */
+#define I2C_READ              1
+#define I2C_WRITE             0
+#define I2C_HALFCYCLE_DELAY   300
+
+static int i2c_sendbyte(uint8_t b) {
+	/* TBD */
+	return 0;
 }
 
-/* I2C_ISR_BUSY: Bus busy
- * 
- * I2C_ISR_TIMEOUT: Timeout or t_LOW detection flag
- * I2C_ISR_PECERR: PEC Error in reception
- * I2C_ISR_OVR: Overrun/Underrun (slave mode)
- * I2C_ISR_ARLO: Arbitration lost
- * I2C_ISR_BERR: Bus error
- * 
- * I2C_ISR_TCR: Transfer Complete Reload
- * I2C_ISR_TC: Transfer Complete (master mode)
- * I2C_ISR_STOPF: Stop detection flag
- * I2C_ISR_NACKF: Not Acknowledge received flag
- * 
- * I2C_ISR_RXNE: Receive data register not empty (receivers)
- * I2C_ISR_TXIS: Transmit interrupt status (transmitters)
- * I2C_ISR_TXE: Transmit data register empty (transmitters)
- */
-#define I2C_ISR_ERRORMASK      (I2C_ISR_TIMEOUT|I2C_ISR_PECERR|I2C_ISR_OVR|I2C_ISR_ARLO|I2C_ISR_BERR)
-#define I2C_ICR_CLEARMASK      (I2C_ICR_ALERTCF|I2C_ICR_TIMOUTCF|I2C_ICR_PECCF|I2C_ICR_OVRCF|I2C_ICR_ARLOCF| \
-                                I2C_ICR_BERRCF|I2C_ICR_STOPCF|I2C_ICR_NACKCF|I2C_ICR_ADDRCF)
-#define I2C_SOFT_TIMEOUT       65536 /* assuming 48MHz, 5 cycles per loop: 65536*5*20.83ns = ~6.8ms */
-static uint32_t i2c_status_wait(uint32_t flags) {
-	uint32_t sr, soft_timeout=I2C_SOFT_TIMEOUT;
-	for(;soft_timeout;soft_timeout--) {
-		sr = I2C_ISR(I2C1);
-		if(sr&I2C_ISR_ERRORMASK)
-			break;
-		if(sr&I2C_ISR_BUSY)
-			continue;
-		if((sr&flags) || (!flags)) /* return if at least 1 flag matches */
-			break;
+/* gpio_get(I2C_PORT, I2C_SCL|I2C_SDA) */
+
+static int i2c_start(uint8_t addr) {
+	while(1) {
+		gpio_clear(I2C_PORT, I2C_SCL|I2C_SDA);
+		udelay(I2C_HALFCYCLE_DELAY);
+		gpio_set(I2C_PORT, I2C_SCL|I2C_SDA);
+		udelay(I2C_HALFCYCLE_DELAY);
 	}
-	return sr;
+	/* TBD */
+	return 0;
 }
 
-#define I2C_READ    1
-#define I2C_WRITE   0
-
-static void i2c_start(uint8_t addr, uint8_t read_nwrite, size_t n) {
-	i2c_set_7bit_address(I2C1, addr);
-	if(read_nwrite == I2C_READ)
-		i2c_set_read_transfer_dir(I2C1);
-	else
-		i2c_set_write_transfer_dir(I2C1);
-	i2c_set_bytes_to_transfer(I2C1, MIN(n,255));
-	if(n>255)
-		I2C_CR2(I2C1) |= I2C_CR2_RELOAD;
-	else
-		I2C_CR2(I2C1) &= ~I2C_CR2_RELOAD;
-	i2c_send_start(I2C1);
+static int i2c_rcvbyte(uint8_t *b, uint8_t ack) {
+	/* TBD */
+	return 0;
 }
 
-#define I2C_TX_STATUSMASK     (I2C_ISR_NACKF | I2C_ISR_TXIS)
-
-/* this function should resemble the flow-charts from figures 231, 232 of RM0091 */
-static int i2c_write(uint8_t addr, const uint8_t *d, size_t n) {
-	uint32_t nbytes = MIN(n,255), reload = n>255;
-
-	i2c_start(addr,I2C_WRITE,n);
-
-	while(n) {
-		uint32_t sr = i2c_status_wait(I2C_TX_STATUSMASK);
-		if(sr != I2C_ISR_TXIS)
-			return 0;
-		i2c_send_data(I2C1, *d++);
-		n--;
-		nbytes--;
-		if(nbytes) /* continue until current chunk complete */
-			continue;
-		else { /* current chunk complete */
-			sr = i2c_status_wait(I2C_ISR_TC|I2C_ISR_TCR);
-			if(!(sr & I2C_ISR_TC)) /* TC not set -> error */
-				return 0;
-			if(!reload) /* last chunk -> done */
-				return 1;
-			/* not last chunk - expect TCR set */
-			if(!(sr & I2C_ISR_TCR)) /* TCR not set -> error */
-				return 0;
-			/* next chunk */
-			nbytes =  MIN(n,255);
-			i2c_set_bytes_to_transfer(I2C1, nbytes);
-			reload = n>255;
-			if(reload)
-				I2C_CR2(I2C1) |= I2C_CR2_RELOAD;
-			else
-				I2C_CR2(I2C1) &= ~I2C_CR2_RELOAD;
-		} /* current nbytes chunk complete */
-	} /* foreach byte to write */
-	
-	return 0; /* shouldn't be reached */
+static void i2c_stop(void) {
+	/* TBD */
 }
 
-static int i2c_read(uint8_t addr, uint8_t *d, size_t n) {
-	uint32_t nbytes = MIN(n,255), reload = n>255;
-
-	i2c_start(addr,I2C_READ,n);
-
-	while(n) {
-		uint32_t sr = i2c_status_wait(I2C_ISR_RXNE);
-		if(sr != I2C_ISR_RXNE)
-			return 0;
-		*d = i2c_get_data(I2C1);
-		d++;
-		n--;
-		nbytes--;
-		if(nbytes) /* continue until current chunk complete */
-			continue;
-		else { /* current chunk complete */
-			sr = i2c_status_wait(I2C_ISR_TC|I2C_ISR_TCR);
-			if(!(sr & I2C_ISR_TC)) /* TC not set -> error */
-				return 0;
-			if(!reload) /* last chunk -> done */
-				return 1;
-			/* not last chunk - expect TCR set */
-			if(!(sr & I2C_ISR_TCR)) /* TCR not set -> error */
-				return 0;
-			/* next chunk */
-			nbytes =  MIN(n,255);
-			i2c_set_bytes_to_transfer(I2C1, nbytes);
-			reload = n>255;
-			if(reload)
-				I2C_CR2(I2C1) |= I2C_CR2_RELOAD;
-			else
-				I2C_CR2(I2C1) &= ~I2C_CR2_RELOAD;
-		} /* current nbytes chunk complete */
-	} /* foreach byte to write */
-	
-	return 0; /* shouldn't be reached */
-}
-
+/* Errata sheet ES0243 rev 3 lists several horrible errata for the hardware I2C peripheral
+ * therefor a software I2C implementation is used */
 int i2c_xfer(uint8_t addr, const void *wr_p, size_t wr_sz, void *rd_p, size_t rd_sz) {
 	const uint8_t *wr_d = wr_p;
 	uint8_t *rd_d = rd_p;
 	int n=0;
 
-	/* wait if i2c peripheral still busy */
-	i2c_status_wait(0);
-
-	/* do a sort-of soft reset if error flag(s) set
-	 * resets state machine and all error flags */
-	if(I2C_ISR(I2C1) & I2C_ISR_ERRORMASK) {
-		int i;
-		i2c_peripheral_disable(I2C1);
-		/* RM0091: PE must be kept low for at least 3 APB clock cycles */
-		for(i=5;i;i--) {
-			uint32_t dummy = I2C_ISR(I2C1);
-			dummy=dummy; /* silence the compiler warning */
-		}
-		i2c_peripheral_enable(I2C1);
-	}
-	
-	/* clear all flags */
-	I2C_ICR(I2C1) = I2C_ICR_CLEARMASK;
-
 	/* write cycle */
 	if((wr_d) && (wr_sz>0)) {
-		int res = i2c_write(addr, wr_p, wr_sz);
+		int res = i2c_start((addr<<1)|I2C_WRITE);
 		if(!res)
 			goto out;
+		for(;wr_sz;wr_sz--,wr_d++) {
+			res = i2c_sendbyte(*wr_d);
+			if(!res)
+				goto out;
+		}
 		/* no stop here b/c repeated start follows or stop before function return */
 		n++;
 	}
 
 	/* read cycle */
 	if((rd_d) && (rd_sz>0)) {
-		int res = i2c_read(addr, rd_p, rd_sz);
+		int res = i2c_start((addr<<1)|I2C_READ);
 		if(!res)
 			goto out;
+		for(;rd_sz;rd_sz--,rd_d++) {
+			res = i2c_rcvbyte(rd_d, rd_sz > 1);
+			if(!res)
+				goto out;
+		}
 		n++;
 	}
 
 out:
-	i2c_send_stop(I2C1);
+	i2c_stop();
 	return n;
 }
 
@@ -416,5 +303,4 @@ void hw_init(int argc, char **argv) {
 	gpio_setup();
 	spi_setup();
 	uart_setup();
-	i2c_setup();
 }
